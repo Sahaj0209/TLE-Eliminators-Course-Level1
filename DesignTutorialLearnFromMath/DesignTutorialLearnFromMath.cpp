8d27fce63735616171b71e6066f9d2fa9e26c91c
Sahaj0209 <sahajjaiswal71@gmail.com>
2026-06-29 01:16:29 +0530

    1

--- DesignTutorialLearnFromMath/DesignTutorialLearnFromMath.cpp
+++ DesignTutorialLearnFromMath/DesignTutorialLearnFromMath.cpp
@@ -1,27 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> prime(int n){
-    vector<int> v(n+1);
-    v[0]=v[1]=0;
-    for(int i=2;i<=n;i++){
-        if(v[i]!=0){
-            for(int j=i*2;j*j<=n;j+=i){
-                v[j]=0;
-            }
-        }
-    }
-    return v;
-}
 int main(){
     int n;
     cin>>n;
-    int l = 4;
-    int r = n;
-    vector<int> v = prime(n);
-    while(v[l]==1 && v[r-l]==1){
-        l++;
-        r--;
+    if(n%2==0){
+        cout<<4<<" "<<n-4<<endl;
+    } else {
+        cout<<9<<" "<<n-9<<endl;
     }
-    cout<<r<<" "<<l<<endl;
     return 0;
 }
